6a8bf190c375effc36e40230502cef48af669da9
Александр Зайцев <sdjgsinhzoghaodg@yandex.ru>
2025-09-12 13:38:08 +0300

    header html + css

--- src/components/Header/HeaderLayout/HeaderLayout.tsx
+++ src/components/Header/HeaderLayout/HeaderLayout.tsx
@@ -4,13 +4,29 @@ export function HeaderLayout() {
 	
 	return (
 		<header className="header">
-			<div className="container">
+			<div className="header__inner container">
 				<a
 					className="logo"
 					href="/"
 				>
 					Taska
 				</a>
+				<nav className="controls header__controls">
+					<input
+						className="controls__search"
+						id="search"
+						name="search"
+						type='search'
+					/>
+					<select
+						name="filter"
+						id="filter"
+						className="controls__filter"
+					>
+						<option value="default">По умолчанию</option>
+						<option value="alphabet">По алфавиту</option>
+					</select>
+				</nav>
 			</div>
 		</header>
 	);
