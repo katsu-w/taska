70e49ab60523cd850e6d9591ce11c17d7463919d
Александр Зайцев <sdjgsinhzoghaodg@yandex.ru>
2025-09-15 22:05:26 +0300

    small fixes

--- src/hooks/useGetTaskList.ts
+++ src/hooks/useGetTaskList.ts
@@ -3,10 +3,9 @@ import type { ITask } from '../types/types.ts';
 
 export const useGetTaskList = () => {
 	const [taskList, setTaskList] = useState<ITask[]>([]);
-	const [isLoading, setIsLoading] = useState<boolean>(false);
+	const [isLoading, setIsLoading] = useState<boolean>(true);
 	
 	useEffect(() => {
-		setIsLoading(true);
 		fetch('https://jsonplaceholder.typicode.com/todos')
 			.then((loadedData) => loadedData.json())
 			.then((loadedTaskList) => setTaskList(loadedTaskList))
