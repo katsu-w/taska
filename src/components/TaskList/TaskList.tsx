366df4e30c42c135e86ca331497a1c05aff7990b
Александр Зайцев <sdjgsinhzoghaodg@yandex.ru>
2025-09-15 21:12:27 +0300

    integrate loader and jsonplaceholder

--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -1,6 +1,8 @@
 import TaskListLayout from './TaskListLayout';
+import { useGetTaskList } from '../../hooks/useGetTaskList.ts';
 
 export function TaskList() {
+	const {taskList, isLoading} = useGetTaskList();
 	
-	return <TaskListLayout />;
+	return <TaskListLayout taskList={taskList} isLoading={isLoading} />;
 }
--- src/components/TaskList/TaskListLayout/TaskListLayout.tsx
+++ src/components/TaskList/TaskListLayout/TaskListLayout.tsx
@@ -1,24 +1,39 @@
 import './TaskListLayout.scss';
 import Loader from '../../Loader';
+import type { ITask } from '../../../types/types.ts';
 
-export function TaskListLayout() {
+interface ITaskListLayoutProps {
+	taskList: ITask[],
+	isLoading: boolean,
+}
+
+export function TaskListLayout(props: ITaskListLayoutProps) {
+	const { taskList, isLoading } = props;
 	
 	return (
 		<main className='tasklist container'>
-			<Loader />
-			<div className="card">
-				<div className="card__details">
-					<input
-						className="card__status"
-						id="status"
-						name='status'
-						type="checkbox"
-					 
-					/>
-					<p className="card__title">Card titleasdasdasdasdasdsdasdasdasasdasdasd </p>
-				</div>
-				<button className="card__remove-btn btn">Удалить</button>
-			</div>
+			{isLoading ?
+				<Loader />
+				:
+				taskList.map((task) => {
+					return (
+						<div className="card" key={task.id}>
+							<div className="card__details">
+								<input
+									className="card__status"
+									id="status"
+									name='status'
+									type="checkbox"
+									checked={task.completed}
+								/>
+								<p className="card__title">{task.title}</p>
+							</div>
+							<button className="card__remove-btn btn">Удалить</button>
+						</div>
+					)})
+				}
+			
+			
 			<div className="card">
 				<div className="card__details">
 					<input
