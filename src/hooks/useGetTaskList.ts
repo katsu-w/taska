0e36fd0239ca1d809f912a2b8544a2b836d3ce65
Александр Зайцев <sdjgsinhzoghaodg@yandex.ru>
2025-09-15 21:12:27 +0300

    integrate loader and jsonplaceholder

--- src/hooks/useGetTaskList.ts
+++ src/hooks/useGetTaskList.ts
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react';
+import type { ITask } from '../types/types.ts';
 
 export const useGetTaskList = () => {
-	const [taskList, setTaskList] = useState<object[]>([]);
+	const [taskList, setTaskList] = useState<ITask[]>([]);
 	const [isLoading, setIsLoading] = useState<boolean>(false);
 	
 	useEffect(() => {
