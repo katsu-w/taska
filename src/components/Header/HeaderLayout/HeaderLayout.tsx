af8a7ff70660be74ede8a609ac16249270b2b8e7
Александр Зайцев <sdjgsinhzoghaodg@yandex.ru>
2025-09-25 12:46:05 +0300

    add placeholder for search

--- src/components/Header/HeaderLayout/HeaderLayout.tsx
+++ src/components/Header/HeaderLayout/HeaderLayout.tsx
@@ -22,6 +22,7 @@ export function HeaderLayout(props: IHeaderLayoutProps) {
 						id="search"
 						name="search"
 						type="search"
+						placeholder="Поиск.."
 						value={searchValue}
 						onChange={(e) => searchHandler(e.target.value)}
 					/>
